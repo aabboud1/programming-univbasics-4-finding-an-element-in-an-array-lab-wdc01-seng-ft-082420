51729e2751b56bc2a41310f9a3c6c63b0f384998
immutable-hashmap-7791 <abboudahmed1@gmail.com>
2020-08-07 17:50:04 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,8 @@ def find_element_index(array, value_to_find)
   # Add your solution here
   count = 0 
   while count < array.length do
-    puts array.index
+    num = array.index
     count += 1 
   end
+  num
 end
